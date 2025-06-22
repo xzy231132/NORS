a6c6274b89f7554c343abffa9fc6173ff7d598ba
Chris Cocose <chriscocose@icloud.com>
2025-06-22 12:26:47 -0400

    Firebase init.js

---
+++ firebase-init.js
@@ -0,0 +1,18 @@
+
+// firebase-init.js
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyMCOePIbciCf0yTBpIuKd1XF33lRJJUY",
+  authDomain: "onlinerecruitmentsystem-87364.firebaseapp.com",
+  projectId: "onlinerecruitmentsystem-87364",
+  storageBucket: "onlinerecruitmentsystem-87364.firebasestorage.app",
+  messagingSenderId: "882762844501",
+  appId: "1:882762844501:web:91e5957d78db388372c7dc",
+  measurementId: "G-J12S63LX3L"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const db = firebase.firestore();
