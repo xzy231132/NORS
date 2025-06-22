0685624c5ea66740f07b4c765713c6cf56e8c596
xzy231132 <killiangreen@gmail.com>
2025-06-22 16:09:07 -0400

    Create applicant-signup.js
    script that handles user creation

---
+++ applicant-signup.js
@@ -0,0 +1,33 @@
+// commentor: killian green, date: 06/22/25
+// script handles the submission of a user to firestore. records name, email, and pwd.
+// uses an async function for syntax simplicity
+    document.getElementById('applicantSignupForm').addEventListener('submit', async function (event) {
+      event.preventDefault();
+
+      const email = document.getElementById('email').value;
+      const name = document.getElementById('name').value;
+      const password = document.getElementById('password').value;
+      const confirm = document.getElementById('confirm').value;
+      if (password !== confirm) {
+        alert("Passwords do not match. Please try again.");
+        return;
+      }
+      // TODO: encrypt password in some regard (probably hash it?)
+      if (typeof db !== 'undefined') {
+        try {
+          await db.collection('applicants').add({
+            email: email,
+            password: password,
+            name: name
+          });
+          console.log('Applicant successfully recorded to Firestore db');
+          location.href = 'applicant-home.html';
+        } catch (err) {
+          console.error('ERROR, unable to add applicant:', err);
+          alert('Submission of user unsuccessful. Please try again!');
+        }
+      } else {
+        console.error('Firestore db is uninitialized.')
+      }
+
+    });
