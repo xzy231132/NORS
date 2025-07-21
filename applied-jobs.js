8b326e502c8f9e5b5611cd8675769602a6fb770f
xzy231132 <killiangreen@gmail.com>
2025-07-20 23:22:07 -0400

    Update applied-jobs.js

--- applied-jobs.js
+++ applied-jobs.js
@@ -15,16 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
           const data = doc.data();
           const jobId = data.job_id;
           const status = data.status || "Pending";
-          // because this is NoSQL this is going to look weird:
-          // search jobPosts for the document with job_id
-          // get this document, check to make sure it exists, then find the job title or assign a default
-          // then add the job title and your application status to a new line
-          const newPromise = db.collection("jobPosts").doc(jobId).get().then(function(jobDoc) {
+            // because this is NoSQL this is going to look weird:
+            // search jobPost for the document with job_id
+            // get this document, check to make sure it exists, then find the job title or assign a default
+            // then add the job title and your application status to a new line
+          const newPromise = db.collection("jobPost").doc(jobId).get().then(function(jobDoc) {
             const jobTitle = jobDoc.exists ? jobDoc.data().title : "Job Title Missing";
             html += `<li><strong>${jobTitle}</strong> — Status: ${status}</li>`;
           });
-          // push newPromise to the array of promises and then for each one of them spit out a row
-          // with the new info
           promises.push(newPromise);
         });
         Promise.all(promises).then(function() {
